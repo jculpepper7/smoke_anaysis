0875eb72468402ef474f0e041067b048486ee566
jculpepper7 <jculpepper@nevada.unr.edu>
2023-07-26 20:08:38 -0400

    Continuing scripts

--- scr/01_smoke_viz.R
+++ scr/01_smoke_viz.R
@@ -2,13 +2,22 @@ library(tidyverse)
 library(here)
 library(ggridges)
 
-smoke <- read_csv(here('data/monthly_smoke_area.csv'))
 
+# 1. Read data ------------------------------------------------------------
+
+smoke <- read_csv(here('data/monthly_smoke_area.csv'))
 
 summary(smoke)
 
+
+# 2. Clean data -----------------------------------------------------------
+
 smoke_ts <- smoke %>% 
-  mutate(date = with(., sprintf("%d-%02d", year, month)))
+  mutate(date = with(., sprintf("%d-%02d", year, month)),
+         smoke_all_area_km2 = round(smoke_all_area_m2/1000000, digits = 0))
+
+
+# 3. Initial plot ---------------------------------------------------------
 
 ggplot()+
   geom_point(data = smoke_ts, aes(x = date, y = smoke_all_area_percent))+
@@ -16,17 +25,40 @@ ggplot()+
   ylab('Smoke Area (%)')+
   xlab('Date')
 
-#Seasons
-ggplot(smoke_ts, aes(x = smoke_all_area_percent, y = as.factor(year))) +
-  #geom_density_ridges(rel_min_height = 0.005) +
-  geom_density_ridges() +
-  #scale_y_discrete(expand = c(0.01, 0)) +
-  #scale_x_continuous(expand = c(0.01, 0)) +
-  theme_ridges()
+
+# 4. Ridgeline Plot ----------------------------------------------------------
+
+#Ridgeline plot shows area of smoke cover by year
+ggplot(smoke_ts, aes(x = smoke_all_area_km2, y = as.factor(year), group = year, height = ..density..)) +
+  geom_density_ridges(
+    stat = 'binline',
+    binwidth = 50000,
+    draw_baseline = F,
+    rel_min_height = 0,
+    scale = 0.9
+  )+
+  theme_ridges(grid = T)+
+  ylab('Year')+
+  xlab( bquote('Smoke Cover'~(km^2)))+
+  theme(
+    axis.title.x = element_text(hjust = 0.5),
+    axis.title.y = element_text(hjust = 0.5)
+  )
+  
+  
+# 5. Seeasonal Boxplot ----------------------------------------------------
+
+# Add seasons
+
+smoke_ts2 <- smoke_ts %>% 
+  mutate(
+    season = as.factor(if_else(month>=3 & month<=5, 'spring', 
+                     if_else(month>=6 & month<=8, 'summer',
+                             if_else(month>=9 & month<=11, 'fall', 'winter'))))
+  )
+
+# Seasonal boxplot shows area covered by season, colored by year (to see standouts?)
 
 
-ggplot(iris, aes(x = Sepal.Length, y = Species)) +
-  geom_density_ridges(rel_min_height = 0.005) +
-  #scale_y_discrete(expand = c(0.01, 0)) +
-  #scale_x_continuous(expand = c(0.01, 0)) +
-  theme_ridges()
+
+
